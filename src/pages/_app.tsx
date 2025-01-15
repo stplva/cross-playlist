48308d350a1b5e0325d6e02c10f04c6e86d3791c
stplva <sofiia.teplova@gmail.com>
2025-01-15 22:24:02 +0100

    feat: monospace font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
 import 'components/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Inter } from 'next/font/google'
 import AuthProvider from './AuthProvider'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={inter.className}>
+    <main>
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
